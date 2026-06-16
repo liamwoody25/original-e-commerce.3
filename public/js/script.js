1d222df3d61b366f9cbcccd987bcff95346a520e
liam woodward <liamwoodward20@outlook.com>
2026-06-16 09:23:44 +0100

    making updates to javascript

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,65 @@
-const productBtn = document.querySelector('.item-btn');
 const bagBtn = document.querySelector('.bi-bag');
 const bagOffScreen = document.querySelector('.shopping-section');
+const productBtn = document.querySelectorAll('.item-btn');
+const bagContainer = document.querySelector('.cart-content');
 
-function displayBag() {
-  if (bagBtn.classList.toggle('enable')) {
-    bagOffScreen.classList.toggle('enable');
-  } else {
-    bagOffScreen.classList.toggle('enable');
+const products = [
+  {
+    name: 'Macbook',
+    price: 899,
+    image: './public/assets/images/pexels-dlxmedia-hu-maccbook.png',
+    id: 1
+  },
+  // {
+  //   name: 'Android',
+  //   price: 399,
+  //   image: './public/assets/imagesandrey-matveev-msartwatch-unsplash.png',
+  //   id: 2
+  // }
+]
+
+function addProduct(product, i) {
+
+
+   product.id = 1
+
+  let productCard = {
+    [products.id]:products
   }
+
+  console.log(products)
+  
+ 
+
+
+  const cardProduct = document.createElement('article');
+  cardProduct.classList.add('product-card');
+
+  const productName = document.createElement('h3');
+  productName.classList.add('product-title')
+  productName.innerText = products.name
+
+
+  cardProduct.append(productName)
+  bagContainer.appendChild(cardProduct)
+
+
+ 
 }
 
 
-productBtn.addEventListener('click', function(){
-  
+function displayBag() {
+  if (bagBtn.classList.toggle('enable')) {
+     bagOffScreen.classList.toggle('enable');
+  } else {
+    bagOffScreen.classList.toggle('enable');
+  }
+} 
+
+productBtn.forEach(function(btn, i){
+  btn.addEventListener('click', function(){
+    addProduct(products, i)
+  })
 })
 
 bagBtn.addEventListener('click', function(){
