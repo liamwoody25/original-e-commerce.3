14748ef434225acea35efe185292667efd9ba5e6
liam woodward <liamwoodward20@outlook.com>
2026-06-22 17:52:49 +0200

    experimenting in javascript

--- public/js/script.js
+++ public/js/script.js
@@ -1,76 +1,20 @@
-const bagBtn = document.querySelector('.bi-bag');
-const bagOffScreen = document.querySelector('.shopping-section');
-const productBtn = document.querySelectorAll('.item-btn');
-const bagContainer = document.querySelector('.cart-content');
+const productItem = document.getElementsByClassName('item-btn')
 
-const products = [
-  {
-    name: 'Macbook',
-    image: './public/assets/images/pexels-dlxmedia-hu-maccbook.png',
-    price: 899,
-    id: 0
-  },
-  {
-    name: 'Android',
-    image: './public/assets/imagesandrey-matveev-msartwatch-unsplash.png',
-    price: 399,
-    id: 0
-  }
-]
 
-const bagOutput = [
-  {bagQuan: 0, bagTotal: 0}
-]
 
-// this function sends the product to the cart when the purchase btn is clicked
-function sendProductToBag(product, i) {
-  const bagOutput = document.getElementById('bag-output');
-  const displayQuantity = Number(bagOutput.innerText) + 1;
+function addProduct(i) {
+  const productTitle = productItem[i].dataset.title
+  const itemId = productItem[i].dataset.itemId
+  const itemPrice = productItem[i].dataset.price
 
-  if (displayQuantity > 10) {
-    displayQuantity = 0;
-  }
-
-  bagOutput.innerText = displayQuantity
-
-  addProduct(product, i)
+  
+  console.log(productTitle, itemId, itemPrice)
 }
 
 
-// this function is for adding multiple products to the cart
-function addProduct(product) {
-  
-  product.id += 1
-  
-  let productCard = {
-    [product.name]: product
-  }
+for(let i = 0; i < productItem.length; i++){
+  productItem[i].addEventListener('click', function(){
+  addProduct(i)
 
-  productCard[product.name].id
-  
-  console.log(productCard)
-}
-
-
-
-
-function displayBag() {
-  if (bagBtn.classList.toggle('enable')) {
-     bagOffScreen.classList.toggle('enable');
-  } else {
-    bagOffScreen.classList.toggle('enable');
-  }
-} 
-
-
-
-for (let i = 0; i < productBtn.length; i++) {
-   productBtn[i].addEventListener('click', function(){
-    sendProductToBag(products[i], [i])
-    
   })
 }
-
-bagBtn.addEventListener('click', function(){
-  displayBag()
-})
