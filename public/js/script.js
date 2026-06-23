6a9a2b94c2197458e438493ddd5722a10dce59b8
liam woodward <liamwoodward20@outlook.com>
2026-06-23 12:48:48 +0200

    experimenting in javascript inside the add product

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,41 @@
 const productItem = document.getElementsByClassName('item-btn')
+const cartBtn = document.querySelector('.bi-bag');
+const bagContainer = document.querySelector('.cart-content');
+const bagOffScreen = document.querySelector('.shopping-section')
 
 
+
+
+
+const bag = []
 
 function addProduct(i) {
   const productTitle = productItem[i].dataset.title
   const itemId = productItem[i].dataset.itemId
   const itemPrice = productItem[i].dataset.price
+  const cardImg = document.getElementsByClassName('card-image')[0].src
 
-  
-  console.log(productTitle, itemId, itemPrice)
+
+  console.log(productTitle, itemId, itemPrice, cardImg)
+}
+
+function displayBag() {
+  if (cartBtn.classList.toggle('enable')){
+    bagOffScreen.classList.toggle('enable');
+  } else {
+    bagOffScreen.classList.toggle('enable');
+  }
 }
 
 
 for(let i = 0; i < productItem.length; i++){
   productItem[i].addEventListener('click', function(){
   addProduct(i)
-
   })
 }
+
+cartBtn.addEventListener('click', function(){
+ displayBag()
+})
+
+
